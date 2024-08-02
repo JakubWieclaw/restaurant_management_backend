5e0602bddf398eeba5c6dc0e0cd252bdf164f56a
Jakub Januszewski <janir3g@gmail.com>
2024-08-02 18:14:48 +0200

    #13 do not send 200 when error
    (cherry picked from commit 4c5cd5e163258cf347e010944f2b9bc9236474b7)

--- src/main/java/com/example/restaurant_management_backend/controllers/AuthController.java
+++ src/main/java/com/example/restaurant_management_backend/controllers/AuthController.java
@@ -3,7 +3,8 @@ package com.example.restaurant_management_backend.controllers;
 import com.example.restaurant_management_backend.jpa.model.Customer;
 import com.example.restaurant_management_backend.jpa.repositories.CustomerRepository;
 import com.example.restaurant_management_backend.security.JwtUtils;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,12 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
+
+    public AuthController(AuthenticationManager authenticationManager, CustomerRepository customerRepository, PasswordEncoder passwordEncoder, JwtUtils jwtUtils) {
+        this.authenticationManager = authenticationManager;
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+    }
 
     @PostMapping("/register")
     public String registerUser(@RequestBody Customer customer) {
@@ -34,7 +41,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         try {
             Authentication authenticationRequest =
                     new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
@@ -42,12 +49,13 @@ public class AuthController {
                     this.authenticationManager.authenticate(authenticationRequest);
             SecurityContextHolder.getContext().setAuthentication(authenticationResponse);
             if (authenticationResponse.isAuthenticated()) {
-                return jwtUtils.generateToken(authenticationResponse.getName());
+                String token = jwtUtils.generateToken(authenticationResponse.getName());
+                return ResponseEntity.ok(token);
             }
         } catch (Exception e) {
-            return "Invalid username or password";
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
         }
-        return "Login failed";
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
     }
 
     public record LoginRequest(String email, String password) {
