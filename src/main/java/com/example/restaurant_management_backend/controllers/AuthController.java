4c5cd5e163258cf347e010944f2b9bc9236474b7
Jakub Januszewski <janir3g@gmail.com>
2024-08-02 18:14:48 +0200

    #13 do not send 200 when error

--- src/main/java/com/example/restaurant_management_backend/controllers/AuthController.java
+++ src/main/java/com/example/restaurant_management_backend/controllers/AuthController.java
@@ -3,6 +3,8 @@ package com.example.restaurant_management_backend.controllers;
 import com.example.restaurant_management_backend.jpa.model.Customer;
 import com.example.restaurant_management_backend.jpa.repositories.CustomerRepository;
 import com.example.restaurant_management_backend.security.JwtUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -39,7 +41,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         try {
             Authentication authenticationRequest =
                     new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
@@ -47,12 +49,13 @@ public class AuthController {
                     this.authenticationManager.authenticate(authenticationRequest);
             SecurityContextHolder.getContext().setAuthentication(authenticationResponse);
             if (authenticationResponse.isAuthenticated()) {
-                return jwtUtils.generateToken(authenticationResponse.getName());
+                String token = jwtUtils.generateToken(authenticationResponse.getName());
+                return ResponseEntity.ok(token);
             }
         } catch (Exception e) {
-            return "Invalid username or password";
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
         }
-        return "Login failed";
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
     }
 
     public record LoginRequest(String email, String password) {
