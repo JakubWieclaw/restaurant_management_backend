09e5c46949f3ee10671abef9448439c3bf2a4905
Jakub Januszewski <janir3g@gmail.com>
2024-10-19 00:12:14 +0200

    #26 added Swagger opperations

--- src/main/java/com/example/restaurant_management_backend/controllers/TableController.java
+++ src/main/java/com/example/restaurant_management_backend/controllers/TableController.java
@@ -1,7 +1,9 @@
 package com.example.restaurant_management_backend.controllers;
 
 import com.example.restaurant_management_backend.services.TableService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/tables")
+@Validated
 public class TableController {
     private final TableService tableService;
 
+    @Operation(summary = "Save table")
     @PostMapping("/save")
     public void save(@RequestBody test te1) {
         tableService.save(te1.id(), te1.capacity());
--- src/main/java/com/example/restaurant_management_backend/controllers/TableReservationController.java
+++ src/main/java/com/example/restaurant_management_backend/controllers/TableReservationController.java
@@ -3,10 +3,11 @@ package com.example.restaurant_management_backend.controllers;
 import com.example.restaurant_management_backend.dto.PossibleReservationHoursForDay;
 import com.example.restaurant_management_backend.dto.ReservationQueryRequest;
 import com.example.restaurant_management_backend.dto.ReservationRequest;
-import com.example.restaurant_management_backend.exceptions.InvalidReservationException;
 import com.example.restaurant_management_backend.services.TableReservationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +19,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/table-reservation")
 @RequiredArgsConstructor
+@Validated
 public class TableReservationController {
 
     private final TableReservationService tableReservationService;
 
+    @Operation(summary = "Get available hours for a single day")
     @PostMapping("/available-hours/day")
     public ResponseEntity<List<LocalTime>> getAvailableHoursForDay(@RequestBody ReservationQueryRequest queryRequest) {
         List<LocalTime> availableHours = tableReservationService.checkPossibleHoursForDay(
@@ -33,6 +36,7 @@ public class TableReservationController {
         return ResponseEntity.ok(availableHours);
     }
 
+    @Operation(summary = "Get available hours for multiple days")
     @PostMapping("/available-hours/days")
     public ResponseEntity<List<PossibleReservationHoursForDay>> getAvailableHoursForDays(@RequestBody ReservationQueryRequest queryRequest) {
         List<PossibleReservationHoursForDay> availableHoursForDays = tableReservationService.checkPossibleHoursForDays(
@@ -44,20 +48,16 @@ public class TableReservationController {
         return ResponseEntity.ok(availableHoursForDays);
     }
 
-    // Endpoint to make a reservation using RequestBody
+    @Operation(summary = "Make reservation")
     @PostMapping("/make")
-    public ResponseEntity<String> makeReservation(@RequestBody ReservationRequest reservationRequest) {
-        try {
-            tableReservationService.makeReservation(
-                    reservationRequest.getDay(),
-                    reservationRequest.getStartTime(),
-                    reservationRequest.getEndTime(),
-                    reservationRequest.getNumberOfPeople(),
-                    reservationRequest.getCustomerId()
-            );
-            return ResponseEntity.ok("Reservation successful");
-        } catch (InvalidReservationException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<Void> makeReservation(@RequestBody ReservationRequest reservationRequest) {
+        tableReservationService.makeReservation(
+                reservationRequest.getDay(),
+                reservationRequest.getStartTime(),
+                reservationRequest.getEndTime(),
+                reservationRequest.getNumberOfPeople(),
+                reservationRequest.getCustomerId()
+        );
+        return ResponseEntity.noContent().build();
     }
 }
