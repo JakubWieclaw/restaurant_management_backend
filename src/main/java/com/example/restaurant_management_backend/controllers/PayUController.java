13bf0c5b507b3afee68e56b31588f8a2fb0d23d8
Jakub Januszewski <janir3g@gmail.com>
2024-10-24 21:04:32 +0200

    #70 get order details

--- src/main/java/com/example/restaurant_management_backend/controllers/PayUController.java
+++ src/main/java/com/example/restaurant_management_backend/controllers/PayUController.java
@@ -7,12 +7,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -27,8 +25,18 @@ public class PayUController {
     public ResponseEntity<PayUOrderResponse> createOrder(
             @RequestParam("orderId") Long orderId,
             @RequestParam("amount") Double amount) {
-        logger.info("Creating order with orderId: {} and amount: {}", orderId, amount);
+        logger.info("Creating response with orderId: {} and amount: {}", orderId, amount);
         PayUTokenResponseDTO payUToken = payUService.getPayUToken();
-        return ResponseEntity.ok(payUService.createOrder(payUToken.access_token(), amount, orderId));
+        PayUOrderResponse response = payUService.createOrder(payUToken.access_token(), amount, orderId);
+        return ResponseEntity.status(HttpStatus.SEE_OTHER).location(response.redirectUri()).build();
+    }
+
+    @Operation(summary = "Get order details")
+    @GetMapping
+    public ResponseEntity<String> getOrder(
+            @RequestParam("orderId") Long orderId) {
+        logger.info("Checking status of orderId {}", orderId);
+        String payUOrderId = payUService.getPayUOrderId(orderId);
+        return ResponseEntity.ok(payUService.getPayUOrderDetails(payUOrderId));
     }
 }
--- src/main/java/com/example/restaurant_management_backend/payU/PayUService.java
+++ src/main/java/com/example/restaurant_management_backend/payU/PayUService.java
@@ -1,5 +1,8 @@
 package com.example.restaurant_management_backend.payU;
 
+import com.example.restaurant_management_backend.jpa.model.Order;
+import com.example.restaurant_management_backend.services.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,17 +11,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class PayUService {
 
-    public static final String PAYU_URI_PREFIX = "https://secure.snd.payu.com";
-    public static final String PAYU_URL = PAYU_URI_PREFIX + "/pl/standard/user/oauth/authorize";
-    public static final String ORDER_CREATE_URL = PAYU_URI_PREFIX + "/api/v2_1/orders";
-    public static final String CLIENT_ID = "485688";
-    public static final String CUSTOMER_IP = "127.0.0.1";
-    public static final String ORDER_DESCRIPTION = "Restaurant order";
-    public static final String ORDER_CURRENCY = "PLN";
-    public static final String CLIENT_SECRET = "55f94323d6c3c0afa854fe8376108a33";
-    public static final String GRANT_TYPE = "client_credentials";
+    private static final String PAYU_URI_PREFIX = "https://secure.snd.payu.com";
+    private static final String PAYU_URL = PAYU_URI_PREFIX + "/pl/standard/user/oauth/authorize";
+    private static final String ORDERS_URL = PAYU_URI_PREFIX + "/api/v2_1/orders";
+    private static final String CLIENT_ID = "485688";
+    private static final String CUSTOMER_IP = "127.0.0.1";
+    private static final String ORDER_DESCRIPTION = "Restaurant order";
+    private static final String ORDER_CURRENCY = "PLN";
+    private static final String CLIENT_SECRET = "55f94323d6c3c0afa854fe8376108a33";
+    private static final String GRANT_TYPE = "client_credentials";
+    private final OrderService orderService;
 
     public PayUOrderResponse createOrder(String accessToken, Double amount, Long orderId) {
         RestTemplate restTemplate = new RestTemplate();
@@ -38,12 +43,31 @@ public class PayUService {
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
 
-        ResponseEntity<PayUOrderResponse> response = restTemplate.exchange(ORDER_CREATE_URL, HttpMethod.POST, request, PayUOrderResponse.class);
+        ResponseEntity<PayUOrderResponse> response = restTemplate.exchange(ORDERS_URL, HttpMethod.POST, request, PayUOrderResponse.class);
         System.out.println("Response Status: " + response.getStatusCode());
         System.out.println("Response Body: " + response.getBody());
 
         return response.getBody();
+    }
 
+    public String getPayUOrderId(Long orderId) {
+        return orderService.getOrderById(orderId).map(Order::getPayUOrderId).orElse(null);
+    }
+
+    public String getPayUOrderDetails(String payUOrderId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(getPayUToken().access_token()); // TODO: Access token get only once
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(ORDERS_URL + "/" + payUOrderId, HttpMethod.GET, request, String.class);
+        System.out.println("Response Status: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody());
+
+        return response.getBody();
     }
 
     private long convertZlotyToGrosze(Double amount) {
